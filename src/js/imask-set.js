0799e5ca502eaf83aee43c0080bff546659e0bbc
VVervolk <vervolk1234@gmail.com>
2023-02-07 18:03:46 +0200

    test

--- src/js/imask-set.js
+++ src/js/imask-set.js
@@ -1,28 +1,61 @@
 import IMask from 'imask';
 
 function inputMask() {
-    const maskInputs = document.querySelectorAll('.js-inputmask');
-    const maskInputs01 = document.querySelectorAll('.js-inputmask-01');
+  const maskInputs = document.querySelectorAll('.js-inputmask');
+  const maskInputsDays = document.querySelectorAll('.js-inputmask-days');
+  const maskInputs01 = document.querySelectorAll('.js-inputmask-01');
 
-    if (maskInputs.length > 0) {
-        maskInputs.forEach(input => {
-            var maskOptions = {
-                mask: '+{38}(000)000-00-00',
-                lazy: false
-            };
-            var mask = IMask(input, maskOptions);
-        })
-    }
-    ////
-    if (maskInputs01.length > 0) {
-        maskInputs01.forEach(input => {
-            var maskOptions = {
-                mask: '+{80}(000)000-00-00',
-                lazy: false
-            };
-            var mask = IMask(input, maskOptions);
-        })
-    }
-    
+  if (maskInputs.length > 0) {
+    maskInputs.forEach(input => {
+      var maskOptions = {
+        mask: '+{38}(000)000-00-00',
+        lazy: false,
+      };
+      var mask = IMask(input, maskOptions);
+    });
+  }
+  //
+  if (maskInputsDays.length > 0) {
+    maskInputsDays.forEach(input => {
+      var maskOptions = {
+        mask: Number,
+        lazy: false,
+        min: 1,
+        max: 31,
+      };
+      var mask = IMask(input, maskOptions);
+    });
+  }
+
+  ////
+  if (maskInputs01.length > 0) {
+    maskInputs01.forEach(input => {
+      var maskOptions = {
+        mask: 'DD.MM.YYYY',
+        lazy: false,
+        blocks: {
+          YYYY: {
+            mask: IMask.MaskedRange,
+            from: 2022,
+            to: 2023,
+          },
+
+          MM: {
+            mask: IMask.MaskedRange,
+            from: 1,
+            to: 12,
+          },
+
+          DD: {
+            mask: IMask.MaskedRange,
+            from: 1,
+            to: 31,
+          },
+        },
+      };
+      var mask = IMask(input, maskOptions);
+    });
+  }
 }
+
 inputMask();

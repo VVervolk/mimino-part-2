31eb1bb6f71261ab3978741c08a15d2f03543596
VVervolk <vervolk1234@gmail.com>
2023-02-13 17:30:56 +0200

    fix modals, finish header, start hero

--- src/js/imask-set.js
+++ src/js/imask-set.js
@@ -4,6 +4,7 @@ function inputMask() {
   const maskInputs = document.querySelectorAll('.js-inputmask');
   const maskInputsDays = document.querySelectorAll('.js-inputmask-days');
   const maskInputs01 = document.querySelectorAll('.js-inputmask-01');
+  const maskInputsName = document.querySelectorAll('.js-inputmask-name');
 
   if (maskInputs.length > 0) {
     maskInputs.forEach(input => {
@@ -52,6 +53,16 @@ function inputMask() {
             to: 31,
           },
         },
+      };
+      var mask = IMask(input, maskOptions);
+    });
+  }
+  //
+  if (maskInputsName.length > 0) {
+    maskInputsName.forEach(input => {
+      var maskOptions = {
+        mask: /^[а-яА-яa-zA-Z ']*$/,
+        lazy: false,
       };
       var mask = IMask(input, maskOptions);
     });

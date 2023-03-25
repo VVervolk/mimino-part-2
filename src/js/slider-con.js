f0840911426c97fe5aa3f44fb365ea1ee692a247
VVervolk <vervolk1234@gmail.com>
2023-03-25 18:41:21 +0200

    continue build sec "details-of-hotel"

--- src/js/slider-con.js
+++ src/js/slider-con.js
@@ -1,153 +1,3 @@
-// function swiperSlider() {
-//   const miniSlider = document.querySelectorAll('[data-slider="mini-slider"]');
-//   const Slider = document.querySelectorAll('[data-slider="slider"]');
-//   const heroSlider = document.querySelectorAll('[data-slider="hero-slider"]');
-//   if (miniSlider) {
-//     miniSlider.forEach(slider => {
-//       //
-//       let arrowLeft = slider.querySelector('.swiper-button-prev');
-//       let arrowRight = slider.querySelector('.swiper-button-next');
-//       //
-
-//       // let pagination = slider.querySelector('.swiper-pagination');
-//       swiper = new Swiper(slider.querySelector('.swiper'), {
-//         speed: 1500,
-//         // loop: true,
-//         //centeredSlides: true,
-//         centeredSlides: true,
-//         // autoplay: {
-//         //     delay: 3000,
-//         //     disableOnInteraction: false,
-//         // },
-
-//         slidesPerView: 2,
-//         spaceBetween: 20,
-//         // pagination: {
-//         //     el: pagination,
-//         //     clickable: true,
-//         //     renderBullet: function (index, className) {
-//         //         return '<li class="' + className + '"></li>';
-//         //     },
-//         // },
-//         //
-//         navigation: {
-//           nextEl: arrowRight,
-//           prevEl: arrowLeft,
-//         },
-//         //
-//         on: {
-//           transitionStart: function () {
-//             let previousIndex = this.previousIndex;
-//             let previousSlide =
-//               slider.getElementsByClassName('swiper-slide')[previousIndex];
-//             if (previousSlide) {
-//               setTimeout(function () {
-//                 previousSlide.classList.remove('is-play');
-//               }, 1000);
-//             }
-//           },
-//           transitionEnd: function () {
-//             let activeIndex = this.activeIndex;
-//             let activeSlide =
-//               slider.getElementsByClassName('swiper-slide')[activeIndex];
-//             activeSlide.classList.add('is-play');
-//           },
-//         },
-//         breakpoints: {
-//           // when window width is >= 0px
-//           0: {
-//             slidesPerView: 'auto',
-//             // centeredSlides: true,
-//             spaceBetween: 25,
-//           },
-//           // when window width is >= 1280px
-//           1028: {
-//             slidesPerView: 2,
-//             spaceBetween: 40,
-//             centeredSlides: false,
-//           },
-//           // when window width is >= 640px
-//           // 1280: {
-//           //   slidesPerView: 3,
-//           //   spaceBetween: 40,
-//           // },
-//         },
-//       });
-//     });
-//   }
-//   if (Slider) {
-//     Slider.forEach(slider => {
-//       //
-//       let arrowLeft = slider.querySelector('.swiper-button-prev');
-//       let arrowRight = slider.querySelector('.swiper-button-next');
-//       //
-
-//       // let pagination = slider.querySelector('.swiper-pagination');
-//       swiper = new Swiper(slider.querySelector('.swiper'), {
-//         speed: 1500,
-//         // loop: true,
-//         //centeredSlides: true,
-//         centeredSlides: true,
-//         // autoplay: {
-//         //     delay: 3000,
-//         //     disableOnInteraction: false,
-//         // },
-
-//         slidesPerView: 2,
-//         spaceBetween: 20,
-//         // pagination: {
-//         //     el: pagination,
-//         //     clickable: true,
-//         //     renderBullet: function (index, className) {
-//         //         return '<li class="' + className + '"></li>';
-//         //     },
-//         // },
-//         //
-//         navigation: {
-//           nextEl: arrowRight,
-//           prevEl: arrowLeft,
-//         },
-//         //
-//         on: {
-//           transitionStart: function () {
-//             let previousIndex = this.previousIndex;
-//             let previousSlide =
-//               slider.getElementsByClassName('swiper-slide')[previousIndex];
-//             if (previousSlide) {
-//               setTimeout(function () {
-//                 previousSlide.classList.remove('is-play');
-//               }, 1000);
-//             }
-//           },
-//           transitionEnd: function () {
-//             let activeIndex = this.activeIndex;
-//             let activeSlide =
-//               slider.getElementsByClassName('swiper-slide')[activeIndex];
-//             activeSlide.classList.add('is-play');
-//           },
-//         },
-//         breakpoints: {
-//           // when window width is >= 0px
-//           0: {
-//             slidesPerView: 'auto',
-//             // centeredSlides: true,
-//             spaceBetween: 25,
-//           },
-//           // when window width is >= 1280px
-//           1028: {
-//             slidesPerView: 2,
-//             spaceBetween: 40,
-//             centeredSlides: false,
-//           },
-//           // when window width is >= 640px
-//           // 1280: {
-//           //   slidesPerView: 3,
-//           //   spaceBetween: 40,
-//           // },
-//         },
-//       });
-//     });
-//   }
 //   if (heroSlider) {
 //     heroSlider.forEach(slider => {
 //       //
@@ -232,32 +82,12 @@ function swiperSlider() {
       let arrowLeft = slider.querySelector('.swiper-button-prev');
       let arrowRight = slider.querySelector('.swiper-button-next');
 
-      //коментуємо чи видаляемо якщо не потрібно
-      let pagination = slider.querySelector('.swiper-pagination');
-      //коментуємо чи видаляемо якщо не потрібно
-
       let swiper = new Swiper(slider.querySelector('.swiper'), {
-        centeredSlides: true,
+        slidesOffsetBefore: 0,
+        centeredSlides: false,
         centeredSlidesBounds: true,
-        speed: 2000,
+        speed: 1500,
 
-        // автоплей
-        //centeredSlides: true,
-        // autoplay: {
-        //     delay: 3000,
-        //     disableOnInteraction: false,
-        // },
-        slidesPerView: 2, // кількість слайдерів для показу
-        spaceBetween: 20, // відстань між слайдерами
-
-        // крапки пагінації
-        pagination: {
-          el: pagination,
-          clickable: true,
-          renderBullet: function (index, className) {
-            return '<li class="' + className + '"></li>';
-          },
-        },
         // кнопки навігації
         navigation: {
           nextEl: arrowRight,
@@ -288,17 +118,17 @@ function swiperSlider() {
         breakpoints: {
           // when window width is >= 320px
           320: {
-            slidesPerView: 1,
+            slidesPerView: 1.34,
             spaceBetween: 20,
           },
           // when window width is >= 768px
-          768: {
+          1024: {
             slidesPerView: 2,
             spaceBetween: 30,
           },
           // when window width is >= 1200px
           1200: {
-            slidesPerView: 3,
+            slidesPerView: 2,
             spaceBetween: 86,
           },
         },

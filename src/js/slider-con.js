b7838460a4be195efcab7ce8fac499a82773d1d6
VVervolk <vervolk1234@gmail.com>
2023-04-26 21:51:26 +0300

    finish hotel and room

--- src/js/slider-con.js
+++ src/js/slider-con.js
@@ -80,7 +80,7 @@ function swiperSlider() {
   const sliderThreeCardsReverse = document.querySelectorAll(
     '.slider-details-reverse'
   );
-  //const heroSlider = document.querySelectorAll('[data-slider="hero-slider"]');
+  const heroSlider = document.querySelectorAll('.hero-room__slider');
   if (sliderTwoCards) {
     sliderTwoCards.forEach(slider => {
       // кнопки вперед та назад
@@ -338,73 +338,57 @@ function swiperSlider() {
       });
     });
   }
+  if (heroSlider) {
+    heroSlider.forEach(slider => {
+      //
+      let arrowLeft = slider.querySelector('.swiper-button-prev');
+      let arrowRight = slider.querySelector('.swiper-button-next');
+      //
 
-  // if (heroSlider) {
-  //     heroSlider.forEach(slider => {
-  //         //
-  //         let arrowLeft = slider.querySelector('.swiper-button-prev');
-  //         let arrowRight = slider.querySelector('.swiper-button-next');
-  //         //
+      let pagination = slider.querySelector('.swiper-pagination');
+      swiper = new Swiper(slider.querySelector('.swiper'), {
+        speed: 1500,
+        //centeredSlides: true,
+        // autoplay: {
+        //     delay: 3000,
+        //     disableOnInteraction: false,
+        // },
+        loop: true,
+        slidesPerView: 1,
 
-  //         let pagination = slider.querySelector('.swiper-pagination');
-  //         swiper = new Swiper(slider.querySelector('.swiper'), {
-  //             speed: 1500,
-  //             //centeredSlides: true,
-  //             // autoplay: {
-  //             //     delay: 3000,
-  //             //     disableOnInteraction: false,
-  //             // },
-  //             slidesPerView: 1,
-  //             spaceBetween: 20,
-  //             pagination: {
-  //                 el: pagination,
-  //                 clickable: true,
-  //                 renderBullet: function (index, className) {
-  //                     return '<li class="' + className + '"></li>';
-  //                 },
-  //             },
-  //             //
-  //             navigation: {
-  //                 nextEl: arrowRight,
-  //                 prevEl: arrowLeft,
-  //             },
-  //             //
-  //             on: {
-  //                 transitionStart: function () {
-  //                     let previousIndex = this.previousIndex;
-  //                     let previousSlide = slider.getElementsByClassName('swiper-slide')[previousIndex];
-  //                     if (previousSlide) {
-  //                         setTimeout(function () {
-  //                             previousSlide.classList.remove('is-play');
-  //                         }, 1000);
-  //                     }
-  //                 },
-  //                 transitionEnd: function () {
-  //                     let activeIndex = this.activeIndex;
-  //                     let activeSlide = slider.getElementsByClassName('swiper-slide')[activeIndex];
-  //                     activeSlide.classList.add('is-play');
-  //                 },
-  //             },
-  //             breakpoints: {
-  //                 // when window width is >= 320px
-  //                 320: {
-  //                     slidesPerView: 1,
-  //                     spaceBetween: 20,
-  //                 },
-  //                 // when window width is >= 480px
-  //                 768: {
-  //                     slidesPerView: 2,
-  //                     spaceBetween: 30
-  //                 },
-  //                 // when window width is >= 640px
-  //                 1200: {
-  //                     slidesPerView: 3,
-  //                     spaceBetween: 40
-  //                 }
-  //             }
-
-  //         });
-  //     });
-  // }
+        pagination: {
+          el: pagination,
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<li class="' + className + '"></li>';
+          },
+        },
+        //
+        navigation: {
+          nextEl: arrowRight,
+          prevEl: arrowLeft,
+        },
+        //
+        on: {
+          transitionStart: function () {
+            let previousIndex = this.previousIndex;
+            let previousSlide =
+              slider.getElementsByClassName('swiper-slide')[previousIndex];
+            if (previousSlide) {
+              setTimeout(function () {
+                previousSlide.classList.remove('is-play');
+              }, 1000);
+            }
+          },
+          transitionEnd: function () {
+            let activeIndex = this.activeIndex;
+            let activeSlide =
+              slider.getElementsByClassName('swiper-slide')[activeIndex];
+            activeSlide.classList.add('is-play');
+          },
+        },
+      });
+    });
+  }
 }
 window.addEventListener('load', swiperSlider, false);
